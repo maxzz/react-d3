ddca2bf177a09faded14413388321adb9494fa78
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-11 17:42:14 -0700

    WIP. Interpolated shape outer points. add working smooth checkbox

--- src/components/StarD3Interpolated.tsx
+++ src/components/StarD3Interpolated.tsx
@@ -1,40 +1,53 @@
 import React from 'react';
-import { curveCatmullRomOpen, curveLinearClosed, lineRadial, randomUniform } from 'd3';
+import { curveCatmullRomClosed, curveCatmullRomOpen, curveLinearClosed, lineRadial, randomUniform } from 'd3';
 import './Slider.scss';
 
 type ShapeParams = {
     nRays: number;
     iRadius: number;
     oRadius: number;
+    smooth: boolean;
+};
+
+type RandomizeParams = {
+    inner: boolean;
+    outer: boolean;
 };
 
 type InterpolatedShapeProps = {
     shape: ShapeParams;
+    randomize: RandomizeParams;
     showOuter: boolean;
 };
 
 const VIEWBOX_SIZE = 200;
 
 function InterpolatedShape({ shape, showOuter }: InterpolatedShapeProps) {
-    const { nRays, iRadius, oRadius, } = shape;
+    const { nRays, iRadius, oRadius, smooth } = shape;
 
     const path = React.useMemo(() => {
         const step = 2 * Math.PI / (nRays * 2);
         const points: [number, number][] = [];
         for (let i = 0; i < nRays * 2; i++) {
-            points.push([i * step, i % 2 === 0 ? oRadius : iRadius]);
+            //points.push([i * step, i % 2 === 0 ? oRadius : iRadius]);
             //points.push([i * step, randomUniform(oRadius, iRadius)()]);
-            //points.push([i * step, i % 2 === 0 ? randomUniform(iRadius, oRadius)() : iRadius]);
+            points.push([i * step, i % 2 === 0 ? randomUniform(iRadius, oRadius)() : iRadius]);
         }
         const outerPts: [number, number][] = points.filter((_, idx) => idx % 2 === 0).map(([a, r]) => {
             return [r * Math.sin(a), r * -Math.cos(a)];
         });
-        return [lineRadial().curve(curveCatmullRomOpen).curve(curveLinearClosed)(points) || '', outerPts] as const;
-    }, [nRays, iRadius, oRadius]);
+
+        let gen = lineRadial().curve(curveLinearClosed);
+        if (smooth) {
+            gen = gen.curve(curveCatmullRomOpen);
+        }
+
+        return [gen(points) || '', outerPts] as const;
+    }, [nRays, iRadius, oRadius, smooth]);
 
     return (
         <svg className="" stroke="white" strokeWidth="2" fill="currentColor" viewBox={`${-VIEWBOX_SIZE / 2} ${-VIEWBOX_SIZE / 2} ${VIEWBOX_SIZE} ${VIEWBOX_SIZE}`}>
-            <path className="" d={`${path[0]}z`} />
+            <path className="" d={`${path[0]}`} />
 
             {showOuter &&
                 <g stroke="white" strokeWidth=".5" fill="none">
@@ -69,28 +82,44 @@ function StarD3Interpolated() {
     const [nRays, setURays] = React.useState(7);
     const [iRadius, setIRadius] = React.useState(60);
     const [oRadius, setORadius] = React.useState(87);
-    const [showOuter, setShowOuter] = React.useState(false);
+    const [smooth, setSmooth] = React.useState(true);
+    const shape = {
+        nRays,
+        iRadius,
+        oRadius,
+        smooth,
+    };
 
-    const shape = { nRays, iRadius, oRadius, };
+    const [iRandom, setIRandom] = React.useState(false);
+    const [oRandom, setORandom] = React.useState(false);
+    const randomize: RandomizeParams = {
+        inner: iRandom,
+        outer: oRandom,
+    };
+
+    const [showOuter, setShowOuter] = React.useState(false);
 
     return (
         <div className="p-2 flex select-none">
             <div className="w-44 h-44 text-blue-800 bg-blue-400 border-8 border-blue-200"
                 style={{ boxShadow: '#0000001f 0px 0px 3px 1px' }}
             >
-                <InterpolatedShape shape={shape} showOuter={showOuter} />
+                <InterpolatedShape shape={shape} randomize={randomize} showOuter={showOuter} />
             </div>
-            <div className="mx-2 p-2">
-                <Slider label="# Rays" value={nRays} onChange={(v) => setURays(v)} />
-                <Slider label="Inner radius" value={iRadius} onChange={(v) => setIRadius(v)} />
-                <Slider label="Outer radius" value={oRadius} onChange={(v) => setORadius(v)} />
+            <div className="mx-2 p-2 flex flex-col justify-between">
+                <div className="">
+                    <Slider label="# Rays" value={nRays} onChange={(v) => setURays(v)} />
+                    <Slider label="Inner radius" value={iRadius} onChange={(v) => setIRadius(v)} />
+                    <Slider label="Outer radius" value={oRadius} onChange={(v) => setORadius(v)} />
+                </div>
 
-                <Checkbox className="mt-1" label="Show outer" value={showOuter} onChange={setShowOuter} />
+                <div className="">
+                    <Checkbox className="" label="Smooth lines" value={smooth} onChange={setSmooth} />
+                    <Checkbox className="" label="Randomize both" value={iRandom} onChange={setIRandom} />
+                    <Checkbox className="" label="Randomize outer" value={oRandom} onChange={setORandom} />
+                    <Checkbox className="" label="Show outer points" value={showOuter} onChange={setShowOuter} />
+                </div>
 
-                <label className="mt-1 flex items-center text-sm">
-                    <input className="mr-1" type="checkbox" checked={showOuter} onChange={(e) => setShowOuter(e.target.checked)} />
-                    Show outer
-                </label>
             </div>
         </div>
     );
