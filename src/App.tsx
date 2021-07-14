e73ce1c016b95518a90e65ab947ca87b783a8808
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-13 17:42:53 -0700

    Revert "Moved out Line component"
    This reverts commit 4b7d9d9d56bb1d1302a2555d188b84d271b6704d.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,43 @@ import './App.scss';
 import Footer from './components/Footer';
 import HierarchyClassic from './components/HierarchyClassic';
 import StarD3Interpolated from './components/StarD3Interpolated';
-//import BgLines from './components/XtraBgLines';
+
+type LineProps = {
+    angle?: string;
+    offset?: string;
+    color?: string;
+};
+
+const Line = ({ angle = '0deg', offset = '0px, 0px', color }: LineProps) => {
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: 'calc(200vw + 200vh)',
+                height: '1px',
+                transform: `translate(${offset}) rotate(${angle}) translate(-50%, -50%)`,
+                transformOrigin: 'top left',
+                backgroundColor: color,
+            }}
+        />
+    );
+};
 
 function App() {
     return (
         <div className="h-screen bg-green-50 bg-gradient-to-tl from-green-500 to-cyan-500">
+            <div className="absolute inset-0 overflow-hidden z-0">
+                <Line angle="45deg" color="red" />
+            </div>
             <div className="w-full h-full flex flex-col z-10">
                 {/* <div className="h-screen bg-green-50" style={{backgroundColor: '#defeff', backgroundImage: 'linear-gradient(347deg, #defeff 0%, #D2FFE2 100%)'}}> */}
-                {/* <BgLines /> */}
-
                 <div className="flex flex-col items-center">
                     <StarD3Interpolated />
                     <HierarchyClassic />
                 </div>
-
-                <div className="">
+                <div className="h-10">
                     <Footer />
                 </div>
             </div>
