338db56406c61626b04a1d476b8239879ea87ed4
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-13 23:57:16 -0700

    Clean up lines code

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,6 @@ import HierarchyClassic from './components/HierarchyClassic';
 import StarD3Interpolated from './components/StarD3Interpolated';
 //import BgLines from './components/XtraBgLines';
 
-type LineProps = {
-    angle?: string;
-    offset?: string;
-    color?: string;
-};
-
-const Line = ({ angle = '0deg', offset = '0px, 0px', color }: LineProps) => {
-    return (
-        <div
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                width: 'calc(200vw + 200vh)',
-                height: '1px',
-                transform: `translate(${offset}) rotate(${angle}) translate(-50%, -50%)`,
-                transformOrigin: 'top left',
-                backgroundColor: color,
-            }}
-        />
-    );
-};
-
 function App() {
     return (
         <div className="h-screen bg-green-50 bg-gradient-to-tl from-green-500 to-cyan-500">
