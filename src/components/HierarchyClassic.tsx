9a06230b18a9281c77644bce3ba654e234d2a1a6
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-14 18:16:50 -0700

    WIP. HierarchyClassic. colors as CSS custom properties

--- src/components/HierarchyClassic.tsx
+++ src/components/HierarchyClassic.tsx
@@ -23,6 +23,8 @@ function HierarchyClassic() {
 
         svg.select('*').remove(); //Proper temp hack to fix HMR problem.
 
+        svg.style('--line-color', '#00295d');
+
         function graph(_root: d3.HierarchyNode<TreeItemData>, {
             label = (d: TItem) => d.data.id,
             highlight = (d: TItem) => false,
@@ -37,94 +39,91 @@ function HierarchyClassic() {
                 if (d.x < x0) x0 = d.x;
             });
 
-            svg.attr("viewBox", [0, 0, width, x1 - x0 + nodeDX * 2] as any)
-                .style("overflow", "visible");
+            svg.attr('viewBox', [0, 0, width, x1 - x0 + nodeDX * 2] as any)
+                .style('overflow', 'visible');
 
-            const g = svg.append("g")
-                .attr("font-family", "sans-serif")
-                .attr("font-size", 10)
-                .attr("transform", `translate(${marginLeft},${nodeDX - x0})`);
+            const g = svg.append('g')
+                .attr('font-family', 'sans-serif')
+                .attr('font-size', 10)
+                .attr('transform', `translate(${marginLeft},${nodeDX - x0})`);
 
             // lines
-            const link = g.append("g")
-                .attr("fill", "none")
-                .attr("stroke", "green")
-                .attr("stroke-opacity", 0.4)
-                .attr("stroke-width", 1.2)
-                .selectAll("path")
+            const link = g.append('g')
+                .attr('fill', 'none')
+                .attr('stroke', 'var(--line-color)')
+                .attr('stroke-opacity', 0.4)
+                .attr('stroke-width', 1.2)
+                .selectAll('path')
                 .data(root.links())
-                .join("path")
-                .attr("stroke", (d) => highlight(d.source) && highlight(d.target) ? "red" : null)
-                .attr("stroke-opacity", (d) => highlight(d.source) && highlight(d.target) ? 1 : null)
-                .attr("d", treeLink as any);
+                .join('path')
+                .attr('stroke', (d) => highlight(d.source) && highlight(d.target) ? 'red' : null)
+                .attr('stroke-opacity', (d) => highlight(d.source) && highlight(d.target) ? 1 : null)
+                .attr('d', treeLink as any);
 
             // circle and text
-            const node = g.append("g")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke-width", 3)
-                .selectAll("g")
+            const node = g.append('g')
+                .attr('stroke-linejoin', 'round')
+                .attr('stroke-width', 3)
+                .selectAll('g')
                 .data(root.descendants())
-                .join("g")
-                .attr("transform", (d) => `translate(${d.y},${d.x})`);
+                .join('g')
+                .attr('transform', (d) => `translate(${d.y},${d.x})`);
 
             // circle
-            node.append("circle")
-                .attr("fill", (d) => highlight(d) ? "red" : d.children ? "#5a57" : "none")
-                .attr("stroke", (d) => highlight(d) ? "red" : d.children ? "#585" : "#5a5")
-                .attr("stroke-width", (d) => d.children ? .7 : 1)
-                .attr("r", 4);
+            node.append('circle')
+                .attr('fill', (d) => highlight(d) ? 'red' : d.children ? '#5a57' : 'none')
+                .attr('stroke', (d) => highlight(d) ? 'red' : d.children ? '#585' : '#5a5')
+                .attr('stroke-width', (d) => d.children ? .7 : 1)
+                .attr('r', 4);
 
             // text
-            node.append("text")
-                .attr("fill", (d: TItem) => highlight(d) ? "red" : 'blue')
-                .attr("dy", "0.32em")
-                .attr("x", (d: TItem) => d.children ? -6 : 6)
-                .attr("text-anchor", (d: TItem) => d.children ? "end" : "start")
-                .text(label)
-                .clone(true).lower()
-                .attr("stroke-width", 1.7)
-                .attr("stroke", "#4aff8780");
+            node.append('text')
+                .attr('fill', (d: TItem) => highlight(d) ? 'red' : 'blue')
+                .attr('dy', '0.32em')
+                .attr('x', (d: TItem) => d.children ? -6 : 6)
+                .attr('text-anchor', (d: TItem) => d.children ? 'end' : 'start')
+                .text(label)//.clone(true).lower().attr('stroke-width', 1.7).attr('stroke', '#4aff8780');
 
             return svg.node();
         }
 
         const chaos: d3.HierarchyNode<TreeItemData> = d3.stratify<TreeItemData>()([
-            { id: "Chaos" },
-            { id: "Gaia", parentId: "Chaos" },
-            { id: "Eros", parentId: "Chaos" },
-            { id: "Erebus", parentId: "Chaos" },
-            { id: "Tartarus", parentId: "Chaos" },
+            { id: 'Chaos' },
+            { id: 'Gaia', parentId: 'Chaos' },
+            { id: 'Eros', parentId: 'Chaos' },
+            { id: 'Erebus', parentId: 'Chaos' },
+            { id: 'Tartarus', parentId: 'Chaos' },
 
-            { id: "Tartarus 1", parentId: "Tartarus" },
-            { id: "Tartarus 2", parentId: "Tartarus" },
-            { id: "Tartarus 3", parentId: "Tartarus" },
-            { id: "Tartarus 4", parentId: "Tartarus" },
-            { id: "Tartarus 5", parentId: "Tartarus" },
-            { id: "Tartarus 6", parentId: "Tartarus" },
-            { id: "Tartarus 7", parentId: "Tartarus" },
-            { id: "Tartarus 8", parentId: "Tartarus" },
+            { id: 'Tartarus 1', parentId: 'Tartarus' },
+            { id: 'Tartarus 2', parentId: 'Tartarus' },
+            { id: 'Tartarus 3', parentId: 'Tartarus' },
+            { id: 'Tartarus 4', parentId: 'Tartarus' },
+            { id: 'Tartarus 5', parentId: 'Tartarus' },
+            { id: 'Tartarus 6', parentId: 'Tartarus' },
+            { id: 'Tartarus 7', parentId: 'Tartarus' },
+            { id: 'Tartarus 8', parentId: 'Tartarus' },
 
-            { id: "Tartarus 1", parentId: "Tartarus" },
-            { id: "Tartarus 2", parentId: "Tartarus" },
-            { id: "Tartarus 3", parentId: "Tartarus" },
-            { id: "Tartarus 4", parentId: "Tartarus" },
-            { id: "Tartarus 5", parentId: "Tartarus" },
-            { id: "Tartarus 6", parentId: "Tartarus" },
-            { id: "Tartarus 7", parentId: "Tartarus" },
-            { id: "Tartarus 8", parentId: "Tartarus" },
+            { id: 'Tartarus 1', parentId: 'Tartarus' },
+            { id: 'Tartarus 2', parentId: 'Tartarus' },
+            { id: 'Tartarus 3', parentId: 'Tartarus' },
+            { id: 'Tartarus 4', parentId: 'Tartarus' },
+            { id: 'Tartarus 5', parentId: 'Tartarus' },
+            { id: 'Tartarus 6', parentId: 'Tartarus' },
+            { id: 'Tartarus 7', parentId: 'Tartarus' },
+            { id: 'Tartarus 8', parentId: 'Tartarus' },
 
-            { id: "Mountains", parentId: "Gaia" },
-            { id: "Pontus", parentId: "Gaia" },
-            { id: "Uranus", parentId: "Gaia" },
+            { id: 'Mountains', parentId: 'Gaia' },
+            { id: 'Pontus', parentId: 'Gaia' },
+            { id: 'Uranus', parentId: 'Gaia' },
 
-            { id: "Uranus 1", parentId: "Uranus" },
-            { id: "Uranus 2", parentId: "Uranus" },
-            { id: "Uranus 3", parentId: "Uranus" },
-            { id: "Uranus 4", parentId: "Uranus" },
-            { id: "Uranus 5", parentId: "Uranus" },
-            { id: "Uranus 6", parentId: "Uranus" },
-            { id: "Uranus 7", parentId: "Uranus" },
-            { id: "Uranus 8", parentId: "Uranus" },
+            { id: 'Uranus 1', parentId: 'Uranus' },
+            { id: 'Uranus 2', parentId: 'Uranus' },
+            { id: 'Uranus 3', parentId: 'Uranus' },
+            { id: 'Uranus 4', parentId: 'Uranus' },
+            { id: 'Uranus 5', parentId: 'Uranus' },
+            { id: 'Uranus 6', parentId: 'Uranus' },
+            { id: 'Uranus 7', parentId: 'Uranus' },
+            { id: 'Uranus 8', parentId: 'Uranus' },
         ]);
 
         graph(chaos);
