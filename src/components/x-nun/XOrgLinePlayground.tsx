f515f0fc31e2768c5ee6f2d632a7d1371b11438b
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-18 16:19:21 -0700

    WIP.LinePlayground, cannot add children to circle

--- src/components/x-nun/XOrgLinePlayground.tsx
+++ src/components/x-nun/XOrgLinePlayground.tsx
@@ -13,11 +13,7 @@ function initial() {
     };
 
     const CURVEINFO: CurveInfo[] = [
-        { name: 'curveLinear', curve: d3.curveLinear, active: true, lineString: '', group: false, info: 'Interpolates the points using linear segments.' },
-        { name: 'curveBasis', curve: d3.curveBasis, active: true, lineString: '', group: true, info: 'Interpolates the start and end points and approximates the inner points using a B-spline.' },
-        { name: 'curveBasisClosed', curve: d3.curveBasisClosed, active: false, lineString: '', group: false, info: 'Uses a closed B-Spline to approximate the points.' },
-
-        { name: 'curveBundle (ß=0)', curve: d3.curveBundle.beta(0), active: false, lineString: '', group: true, info: 'Same as curveBasis with the addition of a paramter ß which determines how close to a straight line the curve is. If ß=0 the curve is straight.' },
+        { name: 'curveBundle (ß=0)', curve: d3.curveBundle.beta(0), active: false, lineString: '', group: false, info: 'Same as curveBasis with the addition of a paramter ß which determines how close to a straight line the curve is. If ß=0 the curve is straight.' },
         { name: 'curveBundle (ß=0.5)', curve: d3.curveBundle.beta(0.5), active: false, lineString: '', group: false, info: 'Same as curveBasis with the addition of a paramter ß which determines how close to a straight line the curve is.' },
         { name: 'curveBundle (ß=1)', curve: d3.curveBundle.beta(1), active: false, lineString: '', group: false, info: 'Same as curveBasis with the addition of a paramter ß which determines how close to a straight line the curve is. If ß=1 the curve is the same as curveBasis.' },
 
@@ -35,7 +31,11 @@ function initial() {
         { name: 'curveNatural', curve: d3.curveNatural, active: false, lineString: '', group: true, info: 'Interpolates the points with a cubic spline with zero 2nd derivatives at the endpoints.' },
         { name: 'curveStep', curve: d3.curveStep, active: false, lineString: '', group: true, info: 'Interpolates the points with alternating horizontal and vertical linear segments. The vertical segments lie midway between points.' },
         { name: 'curveStepAfter', curve: d3.curveStepAfter, active: false, lineString: '', group: false, info: 'Interpolates the points with alternating horizontal and vertical linear segments. The y value changes after the x value.' },
-        { name: 'curveStepBefore', curve: d3.curveStepBefore, active: false, lineString: '', group: false, info: 'Interpolates the points with alternating horizontal and vertical linear segments. The y value changes before the x value.' }
+        { name: 'curveStepBefore', curve: d3.curveStepBefore, active: false, lineString: '', group: false, info: 'Interpolates the points with alternating horizontal and vertical linear segments. The y value changes before the x value.' },
+
+        { name: 'curveLinear', curve: d3.curveLinear, active: true, lineString: '', group: true, info: 'Interpolates the points using linear segments.' },
+        { name: 'curveBasis', curve: d3.curveBasis, active: true, lineString: '', group: true, info: 'Interpolates the start and end points and approximates the inner points using a B-spline.' },
+        { name: 'curveBasisClosed', curve: d3.curveBasisClosed, active: false, lineString: '', group: false, info: 'Uses a closed B-Spline to approximate the points.' },
     ];
 
     const lineGenerator = d3.line();
@@ -134,6 +134,15 @@ function initial() {
             .attr('stroke', '#aa0000')
             .attr('stroke-width', 2)
             .style('cursor', 'move')
+            .call(function (selection) {
+                let g = selection.append('g')
+                g.append('text')
+                .append('tsapn')
+                .attr('x', 0)
+                .text('1')
+                console.log(g);
+                
+            })
             .call(drag)
             .merge(u)
             .attr('cx', d => d[0])
