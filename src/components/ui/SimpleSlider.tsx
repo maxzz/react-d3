cff49f69f649559f2a6c7fdcc2ffa2ff0ce8826e
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-19 17:17:09 -0700

    WIP. Reduce # of sliders. add labelWidht

--- src/components/ui/SimpleSlider.tsx
+++ src/components/ui/SimpleSlider.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { SliderProps } from './ui-props';
 
-function Slider({ value, onChange, label, min = 0, max = 100, step = 1 }: SliderProps) {
+function Slider({ value, onChange, label, min = 0, max = 100, step = 1, labelWidth }: SliderProps) {
     return (
         <div className="flex items-center text-sm text-gray-800">
-            <div className="w-16">{label}</div>
+            <div style={{ ...(labelWidth && {width: labelWidth }) }}>{label}</div>
             <div className="flex items-center">
                 <input className="ui-slider" type="range" value={value} onChange={(e) => onChange(+e.target.value)} min={min} max={max} step={step} />
             </div>
