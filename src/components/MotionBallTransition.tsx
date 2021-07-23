a69ea07f76f701b379a59dd769b1de221e8b551e
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-23 00:10:55 -0700

    WIP. MotionBallTransition. add axis direction customization

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -1,19 +1,24 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-function AxisX() {
+type AxisProps = {
+    direction: 'axisLeft' | 'axisRight' | 'axisTop' | 'axisBottom';
+};
+
+function AxisX({ direction }: AxisProps) {
     const gEl = useRef<SVGGElement>(null);
 
     useEffect(() => {
         d3render();
-    }, []);
+    }, [direction]);
 
     function d3render() {
         if (gEl.current) {
             let xScale = d3.scaleLinear().domain([0, 20]).range([0, 200]);
-            let axisX = d3.axisBottom(xScale);
+            let axisX = d3[direction](xScale);
 
             d3.select(gEl.current)
+            .attr('transform', `translate(30,0)`)
                 .call(axisX);
         }
     }
@@ -26,9 +31,9 @@ function AxisX() {
 
 function MotionBallTransition() {
     return (
-        <div>
+        <div className="bg-blue-400">
             <svg>
-                <AxisX />
+                <AxisX direction="axisLeft" />
             </svg>
         </div>
     );
