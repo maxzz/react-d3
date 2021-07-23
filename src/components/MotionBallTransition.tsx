7e082c42a706cfd7293ef6b4707bba7b09406f19
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-23 00:46:32 -0700

    WIP. MotionBallTransition. add ScatterPlot component

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -17,14 +17,26 @@ function AxisX({ direction }: AxisProps) {
             let xScale = d3.scaleLinear().domain([0, 20]).range([0, 200]);
             let axisX = d3[direction](xScale);
 
-            d3.select(gEl.current)
-            .attr('transform', `translate(30,0)`)
-                .call(axisX);
+            d3.select(gEl.current).call(axisX);
         }
     }
 
     return (
-        <g ref={gEl}>
+        <g ref={gEl} transform={`translate(30,0)`}>
+        </g>
+    );
+}
+
+const datum = d3.range(100).map(_ => [Math.random(), Math.random()]);
+
+function ScatterPlot({ data }: { data: typeof datum; }) {
+    const xSale = d3.scaleLinear().domain([0, 1]).range([0, 200]);
+    const ySale = d3.scaleLinear().domain([0, 1]).range([0, 200]);
+    return (
+        <g>
+            {data.map(item => (
+                <circle cx={xSale(item[0])} cy={ySale(item[1])} r="5" />
+            ))}
         </g>
     );
 }
@@ -34,6 +46,7 @@ function MotionBallTransition() {
         <div className="bg-blue-400">
             <svg>
                 <AxisX direction="axisLeft" />
+                <ScatterPlot data={datum} />
             </svg>
         </div>
     );
