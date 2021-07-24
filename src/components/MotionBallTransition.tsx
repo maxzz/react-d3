b822df5198ad42eb92c0f1aece81edfef75ba931
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-23 23:24:55 -0700

    WIP. MotionBallTransition. add scales owned by parent

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -3,9 +3,12 @@ import * as d3 from 'd3';
 
 type AxisProps = {
     direction: 'axisLeft' | 'axisRight' | 'axisTop' | 'axisBottom';
+    scale: Scale;
 };
 
-function AxisX({ direction }: AxisProps) {
+type Scale = d3.ScaleLinear<number, number>;
+
+function AxisX({ direction, scale }: AxisProps) {
     const gEl = useRef<SVGGElement>(null);
 
     useEffect(() => {
@@ -14,7 +17,7 @@ function AxisX({ direction }: AxisProps) {
 
     function d3render() {
         if (gEl.current) {
-            let xScale = d3.scaleLinear().domain([0, 20]).range([0, 200]);
+            let xScale = d3.scaleLinear().domain([0, 1]).range([0, 200]);
             let axisX = d3[direction](xScale);
 
             d3.select(gEl.current).call(axisX);
@@ -30,14 +33,12 @@ function AxisX({ direction }: AxisProps) {
 const datum = d3.range(100).map(_ => [Math.random(), Math.random()]);
 type Datum = typeof datum;
 
-function ScatterPlot({ data, width, height }: { data: Datum; width: number, height: number; }) {
-    const xSale = d3.scaleLinear().domain([0, 1]).range([0, width]);
-    const ySale = d3.scaleLinear().domain([0, 1]).range([0, height]);
+function ScatterPlot({ data, width, height, xScale, yScale }: { data: Datum; width: number, height: number; xScale: Scale; yScale: Scale; }) {
     return (
         <g>
             <rect width={width} height={height} fill="#f707" />
             {data.map(item => (
-                <circle cx={xSale(item[0])} cy={ySale(item[1])} r="5" />
+                <circle cx={xScale(item[0])} cy={yScale(item[1])} r="5" />
             ))}
         </g>
     );
@@ -51,12 +52,16 @@ function MotionBallTransition() {
     const margin = { left: inner, top: inner, right: inner, bottom: inner };
     const innerWidth = WIDTH - margin.left - margin.right;
     const innerHeight = HEIGHT - margin.top - margin.right;
+
+    const xScale = d3.scaleLinear().domain([0, 1]).range([0, innerWidth]);
+    const yScale = d3.scaleLinear().domain([0, 1]).range([0, innerHeight]);
+
     return (
         <div className="bg-blue-400">
             <svg width={WIDTH} height={HEIGHT}>
-                <AxisX direction="axisLeft" />
+                <AxisX direction="axisLeft" scale={xScale} />
                 <g transform={`translate(${margin.left},${margin.top})`} style={{ outline: '1px solid red' }}>
-                    <ScatterPlot data={datum} width={innerWidth} height={innerHeight} />
+                    <ScatterPlot data={datum} width={innerWidth} height={innerHeight} xScale={xScale} yScale={yScale} />
                 </g>
             </svg>
         </div>
