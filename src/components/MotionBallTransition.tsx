6eca25e111b8a52d7766affae0439122194c476c
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-24 16:38:34 -0700

    WIP. ChartBars. add stiches use

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import * as d3 from 'd3';
+import { css } from '@stitches/react';
 
 type Datum = number;
-const DATA = d3.range(3).map(_ => Math.random());
+//const DATA = d3.range(100).map(_ => Math.random());
+const DATA = d3.range(3).map((_, i) => i/2);
+
+const style = css({
+    fill: 'none',
+    stroke: 'white',
+    strokeWidth: '1',
+});
 
 function Body() {
     const ref = React.useRef<SVGSVGElement>(null);
 
-    const WIDTH = 400;
+    const WIDTH = 300;
     const HEIGHT = 300;
 
     const margin = { left: 20, top: 20, right: 20, bottom: 20 };
@@ -16,7 +24,7 @@ function Body() {
     const innerHeight = HEIGHT - margin.top - margin.bottom;
 
     const xScale = d3.scaleBand().domain(DATA.map((_, i) => `${i}`)).range([margin.left, margin.left + innerWidth]).paddingInner(.1);
-    const yScale = d3.scaleLinear().domain([0, 1]).range([margin.top, margin.top + innerHeight]);
+    const yScale = d3.scaleLinear().domain([0, 1]).range([margin.top + innerHeight, margin.top]);
 
     function bars(svgEl: SVGSVGElement) {
         const svg = d3.select(svgEl);
@@ -27,11 +35,11 @@ function Body() {
             .data(DATA)
             .enter()
             .append('rect')
+            .attr('class', `bar ${style()}`)
             .attr('x', (d, i): number => xScale('' + i) || 0)
             .attr('y', d => yScale(d))
             .attr('width', xScale.bandwidth())
             .attr('height', d => HEIGHT - yScale(d));
-
     }
 
     React.useEffect(() => {
@@ -40,7 +48,6 @@ function Body() {
 
     return (
         <svg width={WIDTH} height={HEIGHT} viewBox={`0 0 ${WIDTH} ${HEIGHT}`} ref={ref}>
-            <circle cx={100} cy={100} r={5} />
         </svg>
     );
 }
