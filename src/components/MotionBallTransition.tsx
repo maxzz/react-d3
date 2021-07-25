cde166b268bcb40c3c244622df18393c150cce79
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-25 01:31:49 -0700

    WIP. ChartBars. add transition on pos and size

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -18,7 +18,7 @@ type API = {
     update: () => void;
 };
 
-const Body = React.forwardRef(function(_props, refAPI: React.Ref<API>) {
+const Body = React.forwardRef(function (_props, refAPI: React.Ref<API>) {
     const refSvg = React.useRef<SVGSVGElement>(null);
 
     const WIDTH = 300;
@@ -30,13 +30,13 @@ const Body = React.forwardRef(function(_props, refAPI: React.Ref<API>) {
     const innerHeight = HEIGHT - margin.top - margin.bottom;
     const bandTop = margin.top + 50;
 
-    const domain = d3.extent(DATA, d => d) as [number, number];
-
-    const xScale = d3.scaleBand().domain(DATA.map((_, i) => `${i}`)).range([margin.left, margin.left + innerWidth]).paddingInner(.7);
-    const yScale = d3.scaleLinear().domain(domain).range([0, innerHeight - bandTop]);
-    //const yScale = d3.scaleLinear().domain(domain).range([margin.top + innerHeight, margin.top]);
-
     function bars(svgEl: SVGSVGElement) {
+        const domain = d3.extent(DATA, d => d) as [number, number];
+
+        const xScale = d3.scaleBand().domain(DATA.map((_, i) => `${i}`)).range([margin.left, margin.left + innerWidth]).paddingInner(.7);
+        const yScale = d3.scaleLinear().domain(domain).range([0, innerHeight - bandTop]);
+        //const yScale = d3.scaleLinear().domain(domain).range([margin.top + innerHeight, margin.top]);
+
         const svg = d3.select(svgEl);
         //svg.selectAll('g').remove();
 
@@ -52,6 +52,9 @@ const Body = React.forwardRef(function(_props, refAPI: React.Ref<API>) {
             .join('rect')
             .attr('class', `bar ${style()}`)
 
+            .transition()
+            .duration(400)
+
             .attr('x', (d, i): number => xScale('' + i) || 0)
             .attr('y', d => bandTop)
             .attr('width', xScale.bandwidth())
@@ -60,7 +63,6 @@ const Body = React.forwardRef(function(_props, refAPI: React.Ref<API>) {
 
     React.useImperativeHandle(refAPI, () => ({
         update: () => {
-            console.log('33');
             DATA = d3.range(13).map(_ => Math.random());
             refSvg.current && bars(refSvg.current);
         }
