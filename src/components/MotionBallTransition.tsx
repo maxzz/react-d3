aee30d6984ea52d78d3151621937e261f7696117
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-26 02:00:30 -0700

    WIP. Checkbox styling. add tailwind checkbox

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -117,6 +117,17 @@ const Body = React.forwardRef(function ({ sorted = false, nBars = 12 }: BodyProp
     );
 });
 
+// const checkboxTick = css({
+//     .form-tick:checked {
+//         background-image: url(data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e);
+//         border-color: transparent;
+//         background-color: currentColor;
+//         background-size: 100% 100%;
+//         background-position: 50%;
+//         background-repeat: no-repeat;
+//     }    
+// });
+
 function MotionBallTransition() {
     const ref = React.useRef<API>(null);
     const [sorted, setSorted] = React.useState(false);
@@ -132,6 +143,15 @@ function MotionBallTransition() {
                 >
                     <IconRefresh />
                 </button>
+
+                {/* <label className="flex items-center space-x-3">
+                    <input 
+                        type="checkbox" name="checked-demo" value="1" 
+                        className="form-tick appearance-none h-6 w-6 border border-gray-300 rounded-md checked:bg-blue-600 checked:border-transparent focus:outline-none" 
+                    />
+                    <span className="text-gray-900 font-medium">Option 1</span>
+                </label> */}
+
                 <CheckboxSmall label="Sorted" checked={sorted} onChange={setSorted} styles={CheckboxStyles} />
                 <Slider label="N Bars" labelWidth="3.5rem" value={nBars} onChange={(value) => setNBars(value)} step={1} min={2} max={120} />
             </div>
