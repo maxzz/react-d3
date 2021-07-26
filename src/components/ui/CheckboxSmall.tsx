29777ea682e5db223a5776eabebfa011fb183fa4
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-26 01:16:49 -0700

    WIP. Checkbox styling. module.scss. css clean up

--- src/components/ui/CheckboxSmall.tsx
+++ src/components/ui/CheckboxSmall.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from './CheckboxSmall.module.scss';
 
-function CheckboxSmall({label, checked, onChange}: {label: string, checked: boolean, onChange: (value: boolean) => void}) {
+export type CheckboxSmallProps = {
+    label: string,
+    checked: boolean,
+    onChange: (value: boolean) => void;
+    //styles: CSSModuleClasses;
+};
+
+function CheckboxSmall({ label, checked, onChange }: CheckboxSmallProps) {
     const { checkbox, input, icon, frame, tick, text } = styles;
     return (
         <label className={checkbox}>
             <input className={input} type="checkbox" checked={checked} onChange={(event) => onChange(event.target.checked)} />
             <svg className={icon} viewBox="0 0 22 22">
                 <rect className={frame} width="100%" height="100%" x=".5" y=".5" rx="3" />
-                <path className={tick} stroke="#6EA340" fill="none" strokeLinecap="round" strokeWidth="4" d="M4.7 11l5 5 9-9" />
+                <path className={tick} d="M4.7 11l5 5 9-9" />
             </svg>
             <span className={text}>{label}</span>
         </label>
