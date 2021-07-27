4ae3da51c9090dbb335207b2a05847ab749b1a9b
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-26 18:19:01 -0700

    WIP. Checkbox styling. fix react parsing url path due to spaces

--- src/components/MotionBallTransition.tsx
+++ src/components/MotionBallTransition.tsx
@@ -131,7 +131,7 @@ const Body = React.forwardRef(function ({ sorted = false, nBars = 12 }: BodyProp
 
 const twCheckboxTick = css({
 
-    'backgroundImage': "url(data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e)",
+    'backgroundImage': `url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e")`,
     'borderColor': 'transparent',
     'backgroundColor': 'currentColor',
     'backgroundSize': '100% 100%',
@@ -141,11 +141,10 @@ const twCheckboxTick = css({
 });
 
 const TwCheckbox = styled('input', {
-    'backgroundColor': 'red',
     '&:checked': {
-        'backgroundImage': "url(data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e)",
+        'backgroundImage': `url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e")`,
         'borderColor': 'transparent',
-        'backgroundColor': 'green',
+        'backgroundColor': 'currentColor',
         'backgroundSize': '100% 100%',
         'backgroundPosition': '50%',
         'backgroundRepeat': 'no-repeat',
@@ -176,7 +175,13 @@ function MotionBallTransition() {
 
                 <label className="flex items-center space-x-3">
                     <TwCheckbox type="checkbox"
-                        className={`form-tick appearance-none h-6 w-6 border-4 border-green-300 rounded-md checked:bg-blue-600 checked:border-transparent focus:outline-none`} />
+                        className={
+                            `h-5 w-5 appearance-none rounded-md
+                            text-green-600 bg-[#ffffff70] border border-gray-300
+                            checked:bg-blue-600 checked:border-transparent focus:outline-none`
+                        }
+                        checked={sorted} onChange={(event) => setSorted(event.target.checked)}
+                    />
                     {/* <input 
                         type="checkbox" name="checked-demo" value="1" 
                         className={`checked:[${twCheckboxTick()}] form-tick appearance-none h-6 w-6 border border-gray-300 rounded-md checked:bg-blue-600 checked:border-transparent focus:outline-none`} 
