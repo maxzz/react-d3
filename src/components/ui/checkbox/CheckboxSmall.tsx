0f200b8c84c8e629af617844924203865a76ffee
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-27 17:47:32 -0700

    Add update of default checkbox styles

--- src/components/ui/checkbox/CheckboxSmall.tsx
+++ src/components/ui/checkbox/CheckboxSmall.tsx
@@ -9,7 +9,7 @@ function CheckboxSmall({ label, checked, onChange, enabled = true, styles = Chec
             <input className={input} type="checkbox" checked={checked} onChange={(event) => onChange(event.target.checked)} disabled={!enabled} />
             <svg className={icon} viewBox="0 0 22 22">
                 <rect className={frame} width="100%" height="100%" x=".5" y=".5" rx="3" />
-                <path className={tick} d="M4.7 11l5 5 9-9" style={{transform: 'scale(.9) translate(1.2px, 1px)'}} />
+                <path className={tick} d="M4.7 11l5 5 9-9" />
             </svg>
             <span className={text}>{label}</span>
         </label>
