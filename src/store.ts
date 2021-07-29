949114387019e7ad3438882b8c58738bb1b8d119
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-29 16:17:28 -0700

    WIP. zustand store. experimant w/ devtools middleware

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import create from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 namespace BarsChart {
     export type Store = {
@@ -6,14 +7,14 @@ namespace BarsChart {
         sorted: boolean;
         setNBars: (v: number) => void;
         setSorted: (v: boolean) => void;
-    }
+    };
 
-    export const useStore = create<Store>((set, get) => ({
+    export const useStore = create<Store>(devtools((set, get) => ({
         nBars: 14,
         sorted: false,
-        setNBars: (v: number) => set(state => ({...state, nBars: v})),
-        setSorted: (v: boolean) => set(state => ({...state, sorted: v})),
-    }));
+        setNBars: (v: number) => set(state => ({ nBars: v })),
+        setSorted: (v: boolean) => set(state => ({ sorted: v })),
+    })));
 }
 
 BarsChart.useStore.subscribe((state, prevState) => {
