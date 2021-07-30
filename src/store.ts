c52509b412d42c28d69333b04e84acd9c758433c
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-30 01:41:51 -0700

    WIP. zustand store. add multi-store persistence 3

--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,7 @@ namespace Global {
     const APPKEY = 'red3';
     let persist: Record<string, string> = {};
     loadAppKey();
-    
+
     function loadAppKey() {
         try {
             const store = localStorage.getItem(APPKEY);
@@ -13,13 +13,20 @@ namespace Global {
         } catch (error) {
         }
     }
-    
+
     function storeAppKey() {
         localStorage.setItem(APPKEY, JSON.stringify(persist));
     }
 
-    export function getKey(key: string): string {
-        return persist[key] || '';
+    export function getKey<T>(key: string, initialData: T): T {
+        let data = persist[key];
+        if (data !== null) {
+            try {
+                initialData = JSON.parse(data);
+            } catch (error) {
+            }
+        }
+        return initialData;
     }
 
     export function setKey(key: string, data: string) {
@@ -44,28 +51,15 @@ namespace BarsChart {
         nBars: 14,
         sorted: false,
     };
-    loadFrom(Global.getKey(KEY));
 
     export const useStore = create<Store>((set, get) => ({
-        ...initialState,
+        ...Global.getKey(KEY, initialState),
         setNBars: (v: number) => set({ nBars: v }),
         setSorted: (v: boolean) => set({ sorted: v }),
     }));
 
-    function loadFrom(store: string) {
-        try {
-            let obj = JSON.parse(store || '');
-            initialState = { ...obj };
-        } catch (error) {
-        }
-    }
-
-    export function storeTo(state: Store): string {
-        return JSON.stringify(state);
-    }
-
     useStore.subscribe((state) => {
-        Global.setKey(KEY, storeTo(state));
+        Global.setKey(KEY, JSON.stringify(state));
     });
 }
 
