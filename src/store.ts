3d7109391d0d7125ebb75ebc3b7ef669e485aa6e
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-29 18:38:24 -0700

    WIP. zustand store. add store persistence

--- src/store.ts
+++ src/store.ts
@@ -8,27 +8,37 @@ namespace BarsChart {
         setSorted: (v: boolean) => void;
     };
 
-    export const useStore = create<Store>((set, get) => ({
+    let initialState = {
         nBars: 14,
         sorted: false,
+    };
+    loadFrom(localStorage.getItem('red3'));
+
+    export const useStore = create<Store>((set, get) => ({
+        ...initialState,
         setNBars: (v: number) => set({ nBars: v }),
         setSorted: (v: boolean) => set({ sorted: v }),
     }));
+
+    function loadFrom(store: string | null) {
+        try {
+            let obj = JSON.parse(store || '');
+            initialState = { ...obj };
+        } catch (error) {
+        }
+    }
+
+    export function storeTo(state: Store): string {
+        return JSON.stringify(state);
+    }
 }
 
 BarsChart.useStore.subscribe((state, prevState) => {
-    //console.log('store', state, prevState);
+    console.log('store', state, prevState);
+    let data = BarsChart.storeTo(state);
+    localStorage.setItem('red3', data);
 });
 
+// if (import.meta.env.MODE === 'development') { /* This code wont be in production!!! */ }
+
 export default BarsChart;
-
-// console.log('import.meta.env.MODE', import.meta);
-// console.log('import.meta.env.MODE', import.meta.env);
-// console.log('import.meta.env.MODE', import.meta.env.BASE_URL);
-// console.log('import. meta. env.MODE', import.meta.env.MODE);
-
-console.log('a5a1');
-if (import.meta.env.MODE === 'development') {
-    console.log('a5a2'); // This code wont be in production!!!
-}
-console.log('a5a3');
