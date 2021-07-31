0349dba88dd97dce8f7c08e00f980516aa1179e2
Max Zakharzhevskiy <maxzz@msn.com>
2021-07-31 02:51:13 -0700

    WIP.TransitionBall. add pseudo stroke inside

--- src/components/TransitionBall.tsx
+++ src/components/TransitionBall.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import { precisionRound } from 'd3';
 
 function Ball({ x, y }: { x: number, y: number; }) {
     const ref = React.useRef<SVGCircleElement>(null);
@@ -8,16 +9,21 @@ function Ball({ x, y }: { x: number, y: number; }) {
     React.useEffect(() => {
         const ball = d3.select(ref.current);
 
-        ball.transition()
+        ball.transition('move-x')
             .duration(800)
             .ease(d3.easeBounceOut)
             .attr('cx', x)
+            .on('end', () => setRealPos((prev) => ({ x, y: prev.y })));
+
+        ball.transition('move-y')
+            .ease(d3.easeCubicInOut)
             .attr('cy', y)
-            .on('end', () => setRealPos({ x, y }));
+            .on('end', () => setRealPos((prev) => ({ x: prev.x, y })));
+
     }, [x, y]);
 
     return (
-        <circle ref={ref} cx={realPos.x} cy={realPos.y} r={45}>
+        <circle ref={ref} cx={realPos.x} cy={realPos.y} r={20} fill="none" stroke="blue" strokeWidth={6} clipPath="circle()" >
         </circle>
     );
 }
@@ -27,11 +33,11 @@ function TransitionBall() {
     const [onLeft, setOnLeft] = React.useState(true);
 
     return (
-        <div className="w-96 h-96 bg-yellow-100">
-            <svg className="bg-red-100" onClick={() => {
+        <div className="w-[300px] h-[300px] bg-yellow-100">
+            <svg className="bg-red-100 w-full h-full" onClick={() => {
                 setOnLeft(v => !v);
             }}>
-                <Ball x={onLeft ? 20 : 150} y={pos.y} />
+                <Ball x={onLeft ? 20 : 280} y={onLeft ? 20 : 280} />
             </svg>
         </div>
     );
