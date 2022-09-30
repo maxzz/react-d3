9787a93923531f715693ceebc5fdf32db32e75f4
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-29 20:43:34 -0700

    WIP. update package dependencies: add @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '',
-    plugins: [reactRefresh()]
+    plugins: [
+        react(),
+    ],
+    server: {
+        port: 3000,
+    }
 });
