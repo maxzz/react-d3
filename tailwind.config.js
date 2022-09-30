e60cd3b44fc0093a206eb459804e3610563bb978
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-29 20:37:45 -0700

    WIP. update package dependencies: autoprefixer, postcss, tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    mode: 'jit',
     purge: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
-    darkMode: false, // or 'media' or 'class'
+    //darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             backgroundImage: theme => ({
