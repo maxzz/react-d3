053436da456375475ab8fcadc37a66b36feb8774
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-30 01:57:07 -0700

    WIP. app. switch to stitches 1.2.8; add stitches.config.ts

--- src/components/Demo3_FunBarsChart.tsx
+++ src/components/Demo3_FunBarsChart.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
-import { css } from '@stitches/react';
+import { css } from '../stitches.config';
 import Checkbox from './ui/checkbox/Checkbox';
 import ButtonQuick from './ButtonQuick';
 import Slider from './ui/slider/Slider';
--- src/components/ui/checkbox/CheckboxTwStitchesCss.tsx
+++ src/components/ui/checkbox/CheckboxTwStitchesCss.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { css } from '@stitches/react';
+import { css } from '../../../stitches.config';
 import { CheckboxProps } from '../ui-props';
 
 const twCheckboxTick = css({
